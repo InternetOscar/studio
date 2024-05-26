c6fa3bc31792c39eca7a1c53dbf46abcee44e36f
Oscar Dobson-Brown <oscardobsonbrown@outlook.com>
2024-05-26 15:28:26 +0800

    content init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Martian_Mono, Xanh_Mono } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const martian_mono = Martian_Mono({weight: ["300"], subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      <body className="bg-stone-100" style={martian_mono.style}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,46 @@
 import Image from "next/image";
 
+import { Inter, Martian_Mono, Xanh_Mono } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const martian_mono = Martian_Mono({weight: ["300"], subsets: ["latin"]});
+
+const xanh_mono = Xanh_Mono({weight: ["400"], subsets: ["latin"]});
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex">
+      {/* <main className="grid grid-cols-3"> */}
+      <div className="w-[1000px] mx-auto">
+        <div className="mx-auto flex flex-col mt-8 z-0" style={inter.style}>
+        <div className="font-bold text-[40px]" >O-DB.STUDIO</div>
+        <div className="uppercase text-[4rem] leading-none mt-8 " style={xanh_mono.style}> Australian internet experience design & webmaster practice</div>
+        <div className="*:my-5 mx-auto text-lg font-light mt-10 " style={inter.style}>
+          <p>O-DB Studio ("the Studio") of Perth, WA, is a internet design and technology studio building products for businesses and teams. We specialise in coding, desing, branding, and marketing. We are dedicated to helping you develop, launch, and manage your projects.</p>
+          <p>By partnering with the Studio you avoid the process of recruiting in-house developement or design teams. We provide the flexibility to work on projects embedding with you, your team, your company or on a project-by-project basis. </p>
+          <p>At the Studio, we believe in harnessing design processes to create a better internet experience for everyone. Our approach to design combines learning through experiementation, understanding complex systems, and embracing simplicity. While continuously seeking knowledge and learning, we develop a deep understanding of the world and the internet that shapes it. This understanding is essential for creating meaningful, impactful designs that leave lasting results.</p>
+          <p>To develop a great internet experience, we recognise the interconnectedness of people the internet and the importance of creative adaptable and cohesive systems that give indiciduals engaging with the internet a sense of place. We strive to strip away the unncecessary and focus on the necessary to create designs which are both functional and beautiful. It's not simply about aesthetics or makg things pretty. it's about creating experiences that fill a purpose with clarity. We help you be more simple and direct.</p>
+          <p>Above all, we are commited to using our knowledge and skills to create a positive lasting impact on the world. We seek to design solutions that help humanity make progress. Many of our clients work on topics and in industries that align with our values. We are proud to contribute to these important causes through our craft.</p>
+          <p>Thank you for taking the time to learn about O-DB Studio. We look forward to the opportunity to work together and create meaningfully impactful designs and projects that make a difference in this digital space we all share.</p>
         </div>
       </div>
+    </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    {/* </main> */}
+    <div id="contact" className="float-right top-0 w-[400px] p-4 mt-24 *:block h-full border rounded-xl m-4 bg-stone-100 hover:bg-stone-50 transition-all cursor-default z-10" style={martian_mono.style}>
+      <h2 className="uppercase text-4xl leading-none mb-10" style={xanh_mono.style}>Let's Work Together</h2>
+        <p>GET IN TOUCH</p>
+        <p className="text-right">OSCAR DOBSON-BROWN</p>
+        {/* open mail app with default greeting template */}
+        <p>E: <a href="mailto:hello@o-db.studio" className="hover:underline transition-all">hello@o-db.studio</a></p>
+        {/* on desktop copy to clipboard, on mobile open in phone app */}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <p>P: <a href="tel:0493176446" className="hover:underline transition-all">0493 176 446</a></p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
 }
